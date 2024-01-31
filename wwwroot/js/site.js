15df8471618dce6573001265ecddf178e5d5d140
Leo <Leo_Tsai@systemweb.com.tw>
2024-01-31 11:47:41 +0800

    改用填入人名

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function setupAutocomplete() {
+    var availableRecorders = [ "Leo", "長勝", "Allen", "Top", "韋傑" ];
+
+    $("#recorderInput").autocomplete({
+        source: availableRecorders
+    });
+}
+
+// 在頁面加載完成後調用自動完成的設置函數
+$(document).ready(function() {
+    setupAutocomplete();
+});
